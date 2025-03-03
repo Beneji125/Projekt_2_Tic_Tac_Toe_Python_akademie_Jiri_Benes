c41826a35e8b3b9206ce86b2980d1edafe708f9a
Beneji125 <Beneji125@gmail.com>
2025-03-03 17:50:17 +0100

    Úprava kódu aby obsahovala pouze jednu global funkci a ostatní funkce jsou definovány v rámci ní Úprava funkcí player_turn, tak aby byla jen jedna a probíhalo pouze střídání hráčů.

--- Druhy_projekt_Tic_Tac_Toe_Jiri_Benes.py
+++ Druhy_projekt_Tic_Tac_Toe_Jiri_Benes.py
@@ -5,19 +5,26 @@ author: Jiří Beneš
 email: Beneji125@gmail.com
 """
 
-def separator():
+def game_start():
     '''
-    This function creates a separator.
-    '''
-
-    return "=" * 45
-
-def game_introduction():
-    '''
-    This function prints the game introduction.
+    This function starts the game of TIC-TAC-TOE by calling on other functions.
     '''
 
-    print(f'''
+    def separator():
+          '''
+          This function creates a separator to make the code output more readable.
+          '''
+
+          return "=" * 90
+
+
+    def game_introduction():
+        '''
+        This function prints the game introduction 
+        and explains the rules of TIC-TAC-TOE.
+        '''
+
+        print(f'''
 Welcome to Tic Tac Toe
 {separator()}
 GAME RULES:
@@ -31,21 +38,22 @@ marks in a:
 {separator()}
 Let's start the game
 {separator()} ''')
+        
 
-
-grid = {
+    grid = {
     1: " ", 2: " ", 3: " ",
     4: " ", 5: " ", 6: " ",
     7: " ", 8: " ", 9: " "
-}
+    }
 
 
-def grid_print():
-    '''
-    This function prints the grid.
-    '''
+    def grid_print():
+      '''
+      This function prints the grid of the game, 
+      so the players can see the state of the game.
+      '''
 
-    print(f'''
+      print(f'''
 +---+---+---+
 | {grid[1]} | {grid[2]} | {grid[3]} |
 +---+---+---+
@@ -56,97 +64,86 @@ def grid_print():
 ''')
 
 
-def player_X_turn():
-    '''
-    This function goes through the player X turn.
-    '''
-
-    while True:
-        x_choice = input("Player X | Please enter your move number: ")
-
-        if x_choice.isdigit():
-            x_choice = int(x_choice)
-
-            if x_choice in grid and grid[x_choice] == " ":
-                grid[x_choice] = "X"
-                break
-            else:
-                print("Invalid move! The cell is already occupied or not in range 1 to 9.")
-        else:
-            print("Invalid input! Please enter a number between 1 and 9.")
-
-
-def player_O_turn():
-    '''
-    This function goes through the player O turn.
-    '''
-
-    while True:
-        o_choice = input("Player O | Please enter your move number: ")
-
-        if o_choice.isdigit():
-            o_choice = int(o_choice)
-
-            if o_choice in grid and grid[o_choice] == " ":
-                grid[o_choice] = "O"
-                break
-            else:
-                print("Invalid move! The cell is already occupied or not in range 1 to 9.")
-        else:
-            print("Invalid input! Please enter a number between 1 and 9.")
-
-
-def win_checker():
-    '''
-    This function checks if there is a winner.
-    '''
-    if grid[1] == grid[2] == grid[3] != " ":
-        print(f"Player {grid[1]} wins!")
-        return True
-    elif grid[4] == grid[5] == grid[6] != " ":
-        print(f"Player {grid[4]} wins!")
-        return True
-    elif grid[7] == grid[8] == grid[9] != " ":
-        print(f"Player {grid[7]} wins!")
-        return True
-    elif grid[1] == grid[4] == grid[7] != " ":
-        print(f"Player {grid[1]} wins!")
-        return True
-    elif grid[2] == grid[5] == grid[8] != " ":
-        print(f"Player {grid[2]} wins!")
-        return True
-    elif grid[3] == grid[6] == grid[9] != " ":
-        print(f"Player {grid[3]} wins!")
-        return True
-    elif grid[1] == grid[5] == grid[9] != " ":
-        print(f"Player {grid[1]} wins!")
-        return True
-    elif grid[3] == grid[5] == grid[7] != " ":
-        print(f"Player {grid[3]} wins!")
-        return True
-    elif " " not in grid.values():
-        print("It's a draw!")
-        return True
-    return False
+    def win_checker():
+        '''
+        This function checks if there are three same symbols in a line and if yes,
+        declares a winner. If not, it checks if the grid is full and if yes,
+        declares a draw. Otherwise, it continues the game.
+        '''
+        
+        if grid[1] == grid[2] == grid[3] != " ":
+            grid_print()
+            print(f"Player {grid[1]} wins!")
+            return True
+        elif grid[4] == grid[5] == grid[6] != " ":
+            grid_print()
+            print(f"Player {grid[4]} wins!")
+            return True
+        elif grid[7] == grid[8] == grid[9] != " ":
+            grid_print()
+            print(f"Player {grid[7]} wins!")
+            return True
+        elif grid[1] == grid[4] == grid[7] != " ":
+            grid_print()
+            print(f"Player {grid[1]} wins!")
+            return True
+        elif grid[2] == grid[5] == grid[8] != " ":
+            grid_print()
+            print(f"Player {grid[2]} wins!")
+            return True
+        elif grid[3] == grid[6] == grid[9] != " ":
+            grid_print()
+            print(f"Player {grid[3]} wins!")
+            return True
+        elif grid[1] == grid[5] == grid[9] != " ":
+            grid_print()
+            print(f"Player {grid[1]} wins!")
+            return True
+        elif grid[3] == grid[5] == grid[7] != " ":
+            grid_print()
+            print(f"Player {grid[3]} wins!")
+            return True
+        elif " " not in grid.values():
+            print("It's a draw!")
+            return True
+        return False
 
 
-def game_start():
-    '''
-    This function starts the game.
-    '''
+    def player_turn():
+      '''
+      This function goes through the players turns, alternating between player X
+      and player O. It also checks if there is a winner or a draw using the 
+      win_checker definition.
+      '''
+
+      player = "X"
+
+      while True:
+          if win_checker(): 
+              break
+
+          separator()
+          print(f"Player {player}'s turn")
+          grid_print()
+
+          while True: 
+              player_choice = input(f"Player {player} | Please enter your move number in range of 1 to 9 and in an unoccupied cell: ")
+
+              if player_choice.isdigit():
+                  player_choice = int(player_choice)
+
+                  if player_choice in grid and grid[player_choice] == " ":
+                      grid[player_choice] = player
+                      print(separator())  
+                      break  
+                  else:
+                      print("Invalid move! The cell is already occupied or not in range 1 to 9.")
+              else:
+                  print("Invalid input! Please enter a number between 1 and 9.")
+
+          player = "X" if player == "O" else "O"
 
     game_introduction()
-    win = False
-    while win == False:
-        grid_print()
-        win = win_checker()
-        if win == True:
-            break
-        player_X_turn()
-        grid_print()
-        win = win_checker()
-        if win == True:
-            break
-        player_O_turn()
+    player_turn()
 
 game_start()
